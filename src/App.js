8661fb13016ff1fef75495e1bead61ae2c4f5fc1
Corey-Vaughan <coreypvaughan@gmail.com>
2018-04-06 21:27:15 -0400

    add images

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  // Set state
+  state = {
+    guessArray: [],
+    message: "Click a picture to start!",
+    score: 0,
+    highScore: 0
+  };
+
+  
 }
 
 export default App;
